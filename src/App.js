9c38b5f069fad27afd32d25d62eaf5846298c733
maaike1566 <maaike.bommerson@hva.nl>
2023-03-07 16:43:20 +0100

    advanced example (#11)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import './styles/App.css'
-import EmployeeList from './components/Employees'
+import { Children, cloneElement} from "react";
 
-const App = () => {
+const Breadcrumbs = ({ children }) => {
+    const arrayChildren = Children.toArray(children);
 
-  const employees= [
-    { id: 1, firstname: "John", lastname: "Doe", job: "Developer"},
-    { id: 2, firstname: "Anna", lastname: "Tiger", job: "Developer"},
-    { id: 3, firstname: "Jim", lastname: "Lion", job: "UX/UI"}
-  ]
+    return(
+      <ul style={{listStyle: "none", display: "flex"}}>
+          {
+            arrayChildren.map((child, index) => {
+              const isLast = index === arrayChildren.length - 1;
 
-  return(
-      <div>
-        <h1>Employees</h1>
-        <ul>
-           <EmployeeList employees={ employees } /> 
-        </ul>
-      </div>
-  )
+              if (!isLast && !child.props.link){
+                throw new Error(
+                  `BreadcrumbItem child no. ${index + 1} should be passed a 'link' prop`
+                )
+              }
+
+              return(
+                <>
+                {
+                  child.props.link ? (
+                    <li>
+                      <a href={child.props.link}
+                          style={{display: "inline-block", textDecoration: "none"}}>
+                            <div style={{marginRight: "5px"}}>
+                              {child.props.title }
+                            </div>
+                          </a>
+                    </li>
+                  ): <li>
+                        <div style={{ marginRight: "5px"}}>
+                          {child.props.title}
+                        </div>
+                    </li>
+                }
+                {
+                  !isLast && (
+                    <li>
+                      <div style={{ marginRight: "5px"}}>
+                        &gt;
+                      </div>
+                    </li>
+                  )
+                }
+                </>
+              )
+            })
+          }
+      </ul>
+    )
+ 
 }
 
-export default App;
+const BreadcrumbItem = ({ isLast }) => {
+    return(
+      <li style={{color:isLast ? "red" : "blue"}} />
+    )
+}
+
+const App = () => {
+    return(
+      <Breadcrumbs>
+          <BreadcrumbItem link="https://reactjs.org/" title="React" />
+          <BreadcrumbItem link="https://reactjs.org/docs/jsx-in-depth.html" title="JSX in depth" />
+          <BreadcrumbItem title="Children" />
+      </Breadcrumbs>
+    )
+}
+export default App
