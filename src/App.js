1aa5ef2348f1561ee28d94533a5ca5b5da15ebe3
maaike1566 <maaike.bommerson@hva.nl>
2023-03-08 10:34:18 +0100

    State (#11)

--- src/App.js
+++ src/App.js
@@ -1,70 +1,24 @@
-import './styles/App.css'
-import { Children, cloneElement} from "react";
-
-const Breadcrumbs = ({ children }) => {
-    const arrayChildren = Children.toArray(children);
-
-    return(
-      <ul style={{listStyle: "none", display: "flex"}}>
-          {
-            arrayChildren.map((child, index) => {
-              const isLast = index === arrayChildren.length - 1;
-
-              if (!isLast && !child.props.link){
-                throw new Error(
-                  `BreadcrumbItem child no. ${index + 1} should be passed a 'link' prop`
-                )
-              }
-
-              return(
-                <>
-                {
-                  child.props.link ? (
-                    <li>
-                      <a href={child.props.link}
-                          style={{display: "inline-block", textDecoration: "none"}}>
-                            <div style={{marginRight: "5px"}}>
-                              {child.props.title }
-                            </div>
-                          </a>
-                    </li>
-                  ): <li>
-                        <div style={{ marginRight: "5px"}}>
-                          {child.props.title}
-                        </div>
-                    </li>
-                }
-                {
-                  !isLast && (
-                    <li>
-                      <div style={{ marginRight: "5px"}}>
-                        &gt;
-                      </div>
-                    </li>
-                  )
-                }
-                </>
-              )
-            })
-          }
-      </ul>
-    )
- 
-}
-
-const BreadcrumbItem = ({ isLast }) => {
-    return(
-      <li style={{color:isLast ? "red" : "blue"}} />
-    )
-}
+import Profile from './components/Profile.js'
 
 const App = () => {
+
+  const person = {
+      id: 1, 
+      firstname: "John",
+      lastname: "Doe",
+      job: "Developer",
+      projects: [
+        { id: 1, name: "React Frontend Development" },
+        { id: 2, name: "UX / UI Design" },
+        { id: 1, name: "HR Program" },
+      ]
+    }
+
     return(
-      <Breadcrumbs>
-          <BreadcrumbItem link="https://reactjs.org/" title="React" />
-          <BreadcrumbItem link="https://reactjs.org/docs/jsx-in-depth.html" title="JSX in depth" />
-          <BreadcrumbItem title="Children" />
-      </Breadcrumbs>
+      <div>
+        <Profile person={ person } />  
+      </div>
     )
 }
+
 export default App
