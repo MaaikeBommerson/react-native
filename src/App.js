2f74343d7841e930d1b16926a389104c4e210b97
maaike1566 <maaike.bommerson@hva.nl>
2023-03-09 11:59:55 +0100

    useState (#11)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import {BrowserRouter, Routes, Route } from "react-router-dom"
-import Menu from "./views/Menu"
-import Home from "./views/Home"
-import Blog from "./views/Blog"
-import Contact from "./views/Contact"
-import Path from "./views/Path"
-
-import NotFound from "./views/NotFound"
+import { useState } from 'react'
 
 const App = () => {
+    const person = {
+      id: 1,
+      firstname: "John",
+      lastname: "Doe",
+      job: "Developer"
+    }
+
+    const [employee, setEmployee] = useState(person)
+
+    const updateEmployee = ({firstname}) => {
+      setEmployee(prev => ({... prev, firstname}) )
+      console.log(employee)
+    }
+
     return(
-       <BrowserRouter>
-          <Routes>
-           <Route path="/" element={<Menu />}>
-                <Route index element={<Home />} />
-                <Route path="blog" element={<Blog />} />
-                <Route path="contact" element={<Contact />} />
-                <Route path="/data/:slug/:id" element={<Path />} />
-                <Route path="*" element={<NotFound />} />
-            </Route>
-         </Routes>
-       </BrowserRouter>
+       <>
+       <h1>{`${employee.firstname} ${employee.lastname}`}</h1>
+       <button 
+       type="button"
+       onClick={() => updateEmployee({firstname: "Bob"})}>
+        Set firstname to Bob!
+       </button>
+       </>
     )
 }
 
